dc2cf68895d25d30ba7c2f4256e8bafc4c69e10c
Samrat-Deula7 <samratdeula461@gmail.com>
2025-12-26 16:10:31 +0545

    Finished all the interface concepts

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,4 +1,4 @@
-// ## Interface 
+// ## Interface
 
 // Fundamentals and Methods in interfaces.
 
@@ -16,85 +16,103 @@ interface Book {
   printTitle(message: string): string;
 
   // Other way to invoke methods in iterface.
-  printSomething:(someValue:number)=>number;
+  printSomething: (someValue: number) => number;
 }
 
-const deepWork: Book={
-    isbn:123,
-    title:"deep work",
-    author:'cal newport',
-    genre:'self-help',
-    printAuthor() {
-        console.log(this.author);
-    },
-    printTitle(message) {
-        return `${this.title} ${message}`;
-    },
-    // first option
-    // printSomething:function (someValue) {
-    //     return someValue;
-    // },
+const deepWork: Book = {
+  isbn: 123,
+  title: "deep work",
+  author: "cal newport",
+  genre: "self-help",
+  printAuthor() {
+    console.log(this.author);
+  },
+  printTitle(message) {
+    return `${this.title} ${message}`;
+  },
+  // first option
+  // printSomething:function (someValue) {
+  //     return someValue;
+  // },
 
-    // Second option
-    // printSomething:(someValue) => {
-    //     console.log(deepWork.author);
-    //     return someValue;
-    // },
+  // Second option
+  // printSomething:(someValue) => {
+  //     console.log(deepWork.author);
+  //     return someValue;
+  // },
 
-    // Third option
-    printSomething(someValue) {
-        return someValue;
-    },
-}
+  // Third option
+  printSomething(someValue) {
+    return someValue;
+  },
+};
 deepWork.printAuthor();
-const result = deepWork.printTitle('is an awsome book');
+const result = deepWork.printTitle("is an awsome book");
 console.log(result);
 console.log(deepWork.printSomething(55));
 
+// ## Interface merging and Extending  Interface
 
-// ## Interface merging and Extending  Interface 
-
-interface Person{
-    name:string;
-    getDetails():string;
+interface Person {
+  name: string;
+  getDetails(): string;
 }
 
 // Merging interface
 
-interface Person{
-    age:number;
+interface Person {
+  age: number;
 }
 
 // Extending interface
 
-interface Employee extends Person{
-    employeeId:number;
+interface Employee extends Person {
+  employeeId: number;
 }
-interface DogOwner{
-    dogName:string;
-    getDogDetails():string;
-}
-
-const person:Person={
-    name:'Samrat',
-    age:20,
-    getDetails() {
-        return `Name:${this.name}, Age:${this.age}`;
-    },
+interface DogOwner {
+  dogName: string;
+  getDogDetails(): string;
 }
 
-const employee: Employee={
-    name:'Sanjay',
-    age:29,
-    employeeId:101,
-    getDetails() {
-        return `Employee-id:${this.employeeId}, Name:${this.name}, Age:${this.age}`;
-    },
-}
+const person: Person = {
+  name: "Samrat",
+  age: 20,
+  getDetails() {
+    return `Name:${this.name}, Age:${this.age}`;
+  },
+};
+
+const employee: Employee = {
+  name: "Sanjay",
+  age: 29,
+  employeeId: 101,
+  getDetails() {
+    return `Employee-id:${this.employeeId}, Name:${this.name}, Age:${this.age}`;
+  },
+};
 console.log(person.getDetails());
-console.log(employee.getDetails())
+console.log(employee.getDetails());
 
+// Extend to two interfaces.
+interface Manager extends Person, DogOwner {
+  managePeople(): void;
+}
 
+const manager: Manager = {
+  name: "bob",
+  age: 40,
+  dogName: "rex",
+  getDetails() {
+    return `Name:${this.name}, Age:${this.age}`;
+  },
+  getDogDetails() {
+    return `Dog-name:${this.dogName}`;
+  },
+  managePeople(){
+    console.log('Managing people...')
+  },
+};
 
-
-
+console.log(manager.getDetails());
+console.log(manager.getDogDetails());
+manager.managePeople();
