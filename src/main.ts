03f08d8c93e22ce1e1ab505c5d1d4a56511c6ee0
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-15 09:56:54 +0545

    created an simple todo list usign tailwind

--- src/main.ts
+++ src/main.ts
@@ -29,12 +29,12 @@ import './Class.ts'
 import './ClassGetSet.ts'
 import './ClassInterface.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a href="/tasks" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href="/tasks" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
@@ -45,6 +45,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ src/tasks.ts
@@ -0,0 +1,79 @@
+// "!" this is an not null assersion that tells typescript that this element will not have null value
+
+// const btn = document.querySelector<HTMLButtonElement>(".test-btn")!;
+
+// btn.disabled=true;
+
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task={
+    description:string;
+    isCompleted:boolean;
+}
+
+const tasks:Task[]=loadTask();
+
+tasks.forEach(renderTask)
+
+function loadTask():Task[]{
+    const storedTasks=localStorage.getItem('tasks')
+    return storedTasks?JSON.parse(storedTasks):[];
+}
+
+taskForm?.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    const taskDescription=formInput?.value;
+    if(taskDescription){
+        const task:Task={
+            description:taskDescription,
+            isCompleted:false
+        };
+        // add task to list
+        addTask(task);
+        // render tasks
+        renderTask(task)
+        // update local storage
+        updateStorage();
+
+        formInput.value='';
+    }
+    else{
+        
+        alert('Please enter a task description');
+    }
+})
+
+function addTask(task:Task):void{
+    tasks.push(task);
+    console.log(tasks);
+}
+
+function renderTask(task:Task):void{
+    const taskElement=document.createElement('li');
+    taskElement.textContent=task.description;
+
+    // check box
+    const taskCheckbox=document.createElement('input');
+    taskCheckbox.type="checkbox";
+    taskCheckbox.checked=task.isCompleted;
+
+    // toggle checkbox
+    taskCheckbox.addEventListener("change",()=>{
+        task.isCompleted=!task.isCompleted;
+        updateStorage();
+        
+    })
+
+
+
+    taskElement.appendChild(taskCheckbox);
+    taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage():void{
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+}
