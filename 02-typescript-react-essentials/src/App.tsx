be45a0f1273fe8642bcbe4b7a8a98a2cd4cbcdc0
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-17 21:30:50 +0545

    Learning about react with type script

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,7 +1,10 @@
+import Component from './starter/03-state'
+
 function App() {
   return (
     <main>
-      <h2>React & Typescript</h2>
+    
+      <Component/>
     </main>
   );
 }
--- 02-typescript-react-essentials/src/starter/02-props/index.tsx
+++ 02-typescript-react-essentials/src/starter/02-props/index.tsx
@@ -1,8 +1,13 @@
-function Component() {
+import React from "react";
+
+type ComponentProps={ name: string; id: number;children?:React.ReactNode };
+
+function Component(props: ComponentProps){
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h2>Name : {props.name}</h2>
+      <h2>ID : {props.id}</h2>
+      {props.children}
     </div>
   );
 }
