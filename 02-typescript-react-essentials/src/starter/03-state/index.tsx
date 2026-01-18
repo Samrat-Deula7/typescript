1d1bcc68df7d160d4b38af46bdcfa1f24350177b
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-18 18:54:26 +0545

    Learning about state and form event handelling on react with type script

--- 02-typescript-react-essentials/src/starter/03-state/index.tsx
+++ 02-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,8 +1,41 @@
+import { useState } from "react";
+
+type Link={
+  id:number;
+  url:string;
+  text:string;
+}
+const navLinks=[
+  {
+    id:1,
+    url:'some url',
+    text:'some text',
+  },
+  {
+    id:2,
+    url:'some url',
+    text:'some text',
+  },
+  {
+    id:3,
+    url:'some url',
+    text:'some text',
+  }
+]
+
 function Component() {
+  const[text,setText]=useState('shakeAndBake')
+
+  // Type script will infer this type to be never[] so we need to add generic
+  const[list,setList]=useState<string[]>([]);
+  const[links,setLinks]=useState<Link[]>(navLinks);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button className="btn btn-center" onClick={()=>{
+        setText("samrat");
+        setLinks([...links,{id:4,url:'hello',text:'hi'}])
+      }}>Click me</button>
     </div>
   );
 }
--- 02-typescript-react-essentials/src/starter/04-events/index.tsx
+++ 02-typescript-react-essentials/src/starter/04-events/index.tsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
+
+type Person ={
+  name:string;
+}
+
 function Component() {
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+
+
+  const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    console.log(e.target.value);
+    setEmail(e.target.value)
+  }
+
+  const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    // const formData=new FormData(e.target as HTMLFormElement)
+    const formData=new FormData(e.currentTarget)
+    console.log(formData);
+    
+    const data=Object.fromEntries(formData)
+    console.log(data);
+    const text=formData.get('text') as string;
+    const person:Person={name:data.text as string}
+  }
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
+      <h2>Event Example</h2>
+      <form action="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input mb-1"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          name="text"
+        />
+        <input
+          type="email"
+          className="form-input mb-1"
+          value={email}
+          onChange={handleChange}
+          name="email"
+        />
+        <button type="submit" className="btn btn-block">submit</button>
+      </form>
     </div>
   );
 }
