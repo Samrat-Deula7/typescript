3d72be1643a5adca7dff41272a67f7f785c59173
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-22 13:44:44 +0545

    Learned to fetch data using useQuery

--- 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
+++ 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
@@ -1,47 +1,18 @@
-import { useState, useEffect } from "react";
-import { type Tour,tourSchema } from "./types";
 
-function Component() {
-  const [tours,setTours]=useState<Tour[]>([])
-  const [isLoading,setIsLoading]=useState(false)
-  const [isError,setIsError]=useState<string | null>(null)
+import { fetchTours } from "./types";
+import { useQuery } from "@tanstack/react-query";
 
-  useEffect(()=>{
-    const fetchData=async()=>{
-      setIsLoading(true)
-      try {
-        const response=await fetch(url)
-        if(!response.ok){
-          throw new Error(`Failed to fetch tours...`)
-        }
-        const rawData:Tour[] =await response.json()
-     
-        setTours(result.data);
-      } catch (error) {
-        const message =error instanceof Error ? error.message :"There was an error...";
-        setIsError(message);
-      }finally{
-        setIsLoading(false)
-      }
-    };
-    fetchData();
-  },[]);
+function Component(){
+const {isPending,isError,error,data:tours,}=useQuery({
+  queryKey:['tours'],
+  queryFn:fetchTours,
+})
 
-  if(isLoading){
-    return <h3>Loading...</h3>
-  }
-  if(isError){
-    return <h3>Error {isError}...</h3>
-  }
-  return (
-    <div>
-     <h2 className="mb-1">Tours</h2>
-     {tours.map((tour)=>{
-      return <p key={tour.id} className="mb-1">
-        {tour.name}
-      </p>
-     })}
-    </div>
-  );
+if(isPending) return <h2>Loading...</h2>
+if(isError) return <h2>Error...{error.message}</h2>
+return <div><h2 className="mb-1">{tours.map((tour)=>{
+  return <p className="mb-1" key={tour.id}>{tour.name}</p>
+})}</h2></div>
 }
-export default Component;
+
+export default Component
