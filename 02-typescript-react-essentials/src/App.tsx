ef1a6ce0af0ac6b6e806aa064ef73c8c749c024b
Samrat-Deula7 <samratdeula461@gmail.com>
2026-01-25 10:33:38 +0545

    completed project with react and typescript

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,9 +1,10 @@
-import Component from "./starter/09-rtk";
+import Component from "./starter/10-tasks";
+
 
 function App() {
   return (
     <main>
-<Component/>
+      <Component />
     </main>
   );
 }
